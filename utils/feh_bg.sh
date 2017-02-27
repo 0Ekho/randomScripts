2fd72c9fd1fe9b9f3933e7a8f6ad02a33cf68d48
Ekho <0Ekho@users.noreply.github.com>
2017-02-27 14:40:25 +0000

    feh doesn't seem to work with Xfce4

--- utils/feh_bg.sh
+++ utils/feh_bg.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n'
-# sets random background with feh put in a cron for automatic changing
+# sets a random background
 # this is for 4* monitors, will need to be adjusted for more / less
-# backgrounds should be a \n delimited file containing the full paths to the 
-# background images you want
+# backgrounds should be a '\n' delimited file
 # $find -type f /full/path/to/imgdir > backgrounds is a good way to generate it
-# current should be made once with 4* image paths and a '0' (\n delimited)
 location=`dirname $0`
 if [ `cat $location/fb_stop` -eq "0" ]; then
 	declare -a current
@@ -15,7 +13,13 @@ if [ `cat $location/fb_stop` -eq "0" ]; then
 	bg_list=( `cat "$location/backgrounds"` )
 	new_number=$(( $RANDOM % ${#bg_list[@]} ))
 	current[${current[4]}]="${bg_list[$new_number]}"
+	# feh option
 	feh --bg-fill "${current[0]}" "${current[1]}" "${current[2]}" "${current[3]}"
+	# switched to xfce and feh doesn't seem to work on 4.12
+	#xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s "${current[0]}"
+    #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor1/workspace0/last-image -s "${current[1]}"
+    #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor2/workspace0/last-image -s "${current[2]}"
+    #xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor3/workspace0/last-image -s "${current[3]}"
 	printf "${current[0]}\n${current[1]}\n${current[2]}\n${current[3]}\n\
 $(( (${current[4]} + 1) % 4))" > "$location/current"
 fi
