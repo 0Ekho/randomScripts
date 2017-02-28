e3fe510811e3f1c0ebfa1d058d6b17b58001b03e
Ekho <0Ekho@users.noreply.github.com>
2017-02-28 00:58:56 +0000

    More ffmpeg replacements, ~8 second runtime now.

--- utils/i3-pixlock.sh
+++ utils/i3-pixlock.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# requires imagemagick and i3lock
- 
+# requires imagemagick and i3lock (ffmpeg also to use the faster commands)
+# slowly adding ffmpeg to completly replace imagemagick for speed
+
 tmpbg="/tmp/lockscreen.png"
 dir="$HOME/Pictures/lockscreen/"
 
@@ -9,22 +10,27 @@ images=($(find ${dir} -name 'lock_*.png'))
 rand=$(( $RANDOM % ${#images[@]} ))
 pic=${images[$rand]}
 
-# so any menues used to lock have time to close
-sleep 4 
-# ffmpeg is faster than imagemagick
+# so any menus used to lock have time to close (eg, Xfce whiskermenu)
+sleep 1 
+# ffmpeg is faster than imagemagick 
 ffmpeg -hide_banner -v error -f x11grab -video_size 7200x1080 -i $DISPLAY \
 -vframes 1 -y "$tmpbg"
 #import -window root +repage "$tmpbg"
 
-# fix stupid error caused by different sized screens
-convert "$tmpbg" -fill "#090909" -draw "rectangle 5760,0,7200,179" "$tmpbg"
+# fix stupid error caused by different sized screens, you shouldn't need this
+#convert "$tmpbg" -fill "#090909" -draw "rectangle 5760,0,7200,179" "$tmpbg"
+# ffmpeg is faster (~3s vs ~1s)
+ffmpeg -hide_banner -v error -i "$tmpbg" \
+-vf drawbox=5760:0:1440:180:0x090909:t=max -vframes 1 -y "$tmpbg"
 
 #convert "$tmpbg" -scale 10% -scale 1000% -blur 0x8 "$tmpbg"
-convert "$tmpbg" -scale 20% -scale 500% -blur 0x2 "$tmpbg"
+#convert "$tmpbg" -scale 20% -scale 500% -blur 0x3 "$tmpbg"
 #convert "$tmpbg" -scale 10% -scale 1000% -colorize 25% "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% -colorize 10% -blur 0x2 "$tmpbg"
 
 if [ -f "$pic" ]; then
-    convert "$tmpbg" "$pic" -gravity northwest -geometry +1920+0 -composite -matte "$tmpbg"
+    # add overlay image/logo to second screen
+	convert "$tmpbg" "$pic" -gravity northwest -geometry +1920+0 -composite -matte "$tmpbg"
 fi
  
 i3lock -n -e -i "$tmpbg" >> /dev/null &
