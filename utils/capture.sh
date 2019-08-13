e8b22a873b2d1459531ad7665ac225465e470714
Ekho <Ekho@ekho.email>
2019-08-13 19:04:21 +0000

    adjust image commands

--- utils/capture.sh
+++ utils/capture.sh
@@ -3,7 +3,9 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Simple script for capturing screenshots and uploading them or another file
-# to a simple file hosting site. imagemagick required
+# to a simple file hosting site. depends on ffmpeg and exiftool
+# if xfce needs xfce4-screenshooter, else can uncomment xwd and use that instead
+# (but area select will not work currently)
 # you will need to edit to work with your site
 
 
@@ -19,35 +21,35 @@ file_name="SN$(printf %07d "$count")"
 ((count++))
 echo $count > "$folder/counter.txt"
 
-# For my images this seems to be the best compression options, may differ.
-PNG_OPTS=( -define "png:compression-level=9"
-           -define "png:compression-filter=5"
-           -define "png:compression-strategy=1"
-)
-# this mess will use imagemagik to add a timestamp to the image
+# this mess will use ffmpeg to add a timestamp to the image
+# NOTE: do not call this with on non screenshot files, destroys original file
 timestamp()
 {
-    height=$(identify -format %h "$file_path");
-    width=$(identify -format %w "$file_path");
+    fontpath='/usr/share/fonts/truetype/hack/Hack-Regular.ttf'
+    height=$(exiftool -b -ImageHeight "$file_path");
+    width=$(exiftool -b -ImageWidth "$file_path");
     # readable text wont fit on something too small so skip
     if (( height <= 25 )) || (( width <= 100 )); then
         notify-send -t 4000 "Image to small for timestamp to be added."
     else
-        # smaller images get a  smaller font to make things fit better
+        # smaller images get a smaller font to make things fit better
         if (( height <= 250 )) || (( width <= 500 )); then
-            pntsize=8
-            ofst="+2+2"
+            pntsize=12
+            ofst=2
             srkwdt=1
         else
-            pntsize=12
-            ofst="+4+4"
+            pntsize=18
+            ofst=4
             srkwdt=2
         fi
-        timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-        convert "$file_path" -gravity SouthEast -font Noto-Sans -pointsize\
-         "$pntsize" -fill white -stroke black -strokewidth $srkwdt\
-         -annotate "$ofst" "$timestamp" -stroke none -annotate "$ofst"\
-         "$timestamp" "${PNG_OPTS[@]}" "$file_path"
+        timestamp=$(date "+%Y-%m-%d %H\\\\\\:%M\\\\\\:%S")
+        tf=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
+        mv "$file_path" "/tmp/capts_$tf"
+        ffmpeg -i "/tmp/capts_$tf" -vf "$(printf '%s%s%s'\
+         "drawtext=fontfile=${fontpath}:text=${timestamp}:fontsize=${pntsize}"\
+         ":fontcolor=white:borderw=${srkwdt}:bordercolor=black"\
+         ":x=(w-${ofst}-${pntsize}*11):y=(h-${ofst}-${pntsize})")" "$file_path"
+        rm "/tmp/capts_$tf"
     fi
 }
 upload_file_site()
@@ -56,7 +58,7 @@ upload_file_site()
     time_stamp=$(date "+%Y-%m-%d %H:%M:%S")
     notify-send -t 2000 "File is being uploaded..."
     # upload file to site and save response to file and grep url uploaded to
-    response=$(curl -s -F "file=@$file_path" -F "apikey=$apikey" \
+    response=$(curl -s --limit-rate 125K -F "file=@$file_path" -F "apikey=$apikey" \
     https://x88.moe/api/v1/upload.php)
     # get just the url for the file
     url=$(echo "$response" | grep -Po '(?<="url":").*?(?=")')
@@ -97,31 +99,48 @@ for i in "$@"; do
         ;;
         -a|--area)
             file_path="$archive/$file_name.png"
-            import +repage "${PNG_OPTS[@]}" "$file_path"
+            xfce4-screenshooter -r -s "$file_path"
         ;;
         -w|--window)
             file_path="$archive/$file_name.png"
-            import -frame -screen -window "$(xprop -root 32x '\t$0' \
-             _NET_ACTIVE_WINDOW | cut -f 2)" +repage "${PNG_OPTS[@]}" \
-             "$file_path"
+            #rand=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
+            # for some reason pipe break ffmpeg
+            #xwd -id "$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)"\
+            # -out "/tmp/tc_${rand}.xwd"
+            #ffmpeg -i "/tmp/tc_${rand}.xwd" -vframes 1 "$file_path"
+            #rm "/tmp/tc_${rand}.xwd"
+            xfce4-screenshooter -w -s "$file_path"
         ;;
         -x|--fullscreen)
             file_path="$archive/$file_name.png"
-            import -window root +repage "${PNG_OPTS[@]}" "$file_path"
-        # fix error caused by different sized screens, you shouldn't need this
-            ffmpeg -hide_banner -v error -i "$file_path" \
--vf drawbox=5760:0:1440:180:0x090909:t=max -vframes 1 -y "$file_path"
+            #rand=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
+            # for some reason pipe break ffmpeg
+            #xwd -root -out "/tmp/tc_${rand}.xwd"
+            #ffmpeg -i "/tmp/tc_${rand}.xwd" -vframes 1 "$file_path"
+            #rm "/tmp/tc_${rand}.xwd"
+            xfce4-screenshooter -f -s "$file_path"
+            # fix error caused by different sized screens, you shouldn't need
+            # this
+            mv "$file_path" "/tmp/cap_fix.png"
+            ffmpeg -hide_banner -v error -i "/tmp/cap_fix.png"\
+             -vf drawbox=5760:0:1440:180:0x090909:t=fill -vframes 1 -y\
+             "$file_path"
+            rm "/tmp/cap_fix.png"
         ;;
         -t|--area-timestamp)
             file_path="$archive/$file_name.png"
-            import +repage "${PNG_OPTS[@]}" "$file_path"
+            xfce4-screenshooter -r -s "$file_path"
             timestamp
         ;;
         -g|--window-timestamp)
             file_path="$archive/$file_name.png"
-            import -screen -window "$(xprop -root 32x '\t$0' \
-             _NET_ACTIVE_WINDOW | cut -f 2)" +repage "${PNG_OPTS[@]}" \
-             "$file_path"
+            #rand=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
+            # for some reason pipe break ffmpeg
+            #xwd -id "$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)"\
+            # -out "/tmp/tc_${rand}.xwd"
+            #ffmpeg -i "/tmp/tc_${rand}.xwd" -vframes 1 "$file_path"
+            #rm "/tmp/tc_${rand}.xwd"
+            xfce4-screenshooter -w -s "$file_path"
             timestamp
         ;;
         # maybe sometime make clipboard data type automatically checked
@@ -133,16 +152,18 @@ for i in "$@"; do
             file_path="$archive/$file_name.png"
             xclip -selection clipboard -t image/png -o > "$file_path"
         ;;
-        -p|--tmp-capture)
+        -r|--tmp-capture)
             rand=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4)
             file_path="/tmp/tc$rand.png"
-            import -frame -screen -window "$(xprop -root 32x '\t$0' \
-             _NET_ACTIVE_WINDOW | cut -f 2)" +repage "${PNG_OPTS[@]}" \
-             "$file_path"
+            #xwd -id "$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)"\
+            # -out "/tmp/tc_${rand}.xwd"
+            #ffmpeg -i "/tmp/tc_${rand}.xwd" -vframes 1 "$file_path"
+            #rm "/tmp/tc_${rand}.xwd"
+            xfce4-screenshooter -w -s "$file_path"
             echo -n "$file_path" | xclip -i -selection c
             notify-send -t 4000 "File saved to $file_path"
         ;;
-        -p|--copy)
+        -b|--copy)
             copy=1
         ;;
         -s|--share)
@@ -157,7 +178,7 @@ done
 # if no args default to area && share
 if [ -z "${file_path+x}" ]; then
     file_path="$archive/$file_name.png"
-    import +repage "$file_path"
+    xfce4-screenshooter -w -s "$file_path"
     share=1
 fi
 if [ $share -eq 1 ]; then
